1ff6a6cb2014d1b6907c00cd8067f0c7c04450ea
Anjali Mamidi <mamidianjali99@gmail.com>
2024-06-16 15:53:58 -0400

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import Icon from "./components/Icon";
-
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-const itemArray = new Array(9) .fill("empty");
+const itemArray = new Array(9).fill("empty");
 const App = () => {
-  const [isCross,setIsCross] =useState(false);
+  const [isCross,setIsCross] = useState(false);
   const [winMessage,setWinMessage] = useState("");
 
   const reloadGame= () => {
